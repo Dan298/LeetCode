8f1456bfc4abd25c8c5d83096756f35d2abc4145
Daniel Gueta <47797628+Dan298@users.noreply.github.com>
2021-12-02 00:40:35 -0600

    Update coinChange.py

--- coinChange.py
+++ coinChange.py
@@ -1,14 +1,14 @@
 class Solution(object):
     def coinChange(self, coins, amount):
-        total = 0
-        coins.sort()
-        for coin in reversed(coins):
-            temp = amount // coin
-            if temp >= 1:
-                amount = amount - (coin*temp)
-                total += temp
-            if amount == 0:
-                return total
-        return -1
+        coin_array = [amount+1] * (amount+1)
+        coin_array[0] = 0
+        for i in range(1, amount+1):
+            for c in coins:
+                if i >= c:
+                    coin_array[i] = min(coin_array[i], coin_array[i-c] + 1)
+
+        if coin_array[amount] == amount+1:
+            return -1
+        return coin_array[amount]
 S = Solution()
 print(S.coinChange([186,419,83,408],6249))
